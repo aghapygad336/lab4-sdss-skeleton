428c7698d5199090366da65efc2deb495c187f4d
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2020-05-08 16:52:26 +0200

    Lab 4 Computer Networks

--- skeleton_lab4_sdss.py
+++ skeleton_lab4_sdss.py
@@ -5,6 +5,8 @@ import socket
 import time
 import uuid
 import struct
+import time
+import select
 # https://bluesock.org/~willkg/dev/ansi.html
 ANSI_RESET = "\u001B[0m"
 ANSI_RED = "\u001B[31m"
@@ -58,16 +60,23 @@ class NeighborInfo(object):
 neighbor_information = {}
 # Leave the server socket as global variable.
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(('0.0.0.0',0))
+server.listen(20)
 
 # Leave broadcaster as a global variable.
 broadcaster = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Setup the UDP socket
-
+broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+broadcaster.bind(('255.255.255.255',get_broadcast_port()))
 
 def send_broadcast_thread():
     node_uuid = get_node_uuid()
     while True:
         # TODO: write logic for sending broadcasts.
+        broadcaster.sendto(f'{node_uuid} ON {server.getsockname()[1]}'.encode('utf-8'),
+        ('255.255.255.255', get_broadcast_port()))
         time.sleep(1)   # Leave as is.
 
 
@@ -81,26 +90,70 @@ def receive_broadcast_thread():
         # TODO: write logic for receiving broadcasts.
         data, (ip, port) = broadcaster.recvfrom(4096)
         print_blue(f"RECV: {data} FROM: {ip}:{port}")
-
+        data = data.decode('utf-8').split(' ')
+        node, port = data[0], int(data[2])
+        if port != server.getsockname()[1]:
+            # if not same node
+            th1 = daemon_thread_builder(target = exchange_timestamps_thread,args=( node,ip, port)) 
+            th1.start()
+            tcp_server_thread()
 
 def tcp_server_thread():
     """
     Accept connections from other nodes and send them
     this node's timestamp once they connect.
     """
-    pass
+    th = daemon_thread_builder(target=accept_send_ts)
+    th.start()
+            
+def accept_send_ts():
+    while  True:
+        conn, add = server.accept()
+        print_green('connection accepted')
+        timestamp =  time.time()
+        packet = struct.pack('!d',timestamp)
+        conn.send(packet)
+        conn.close()
 
 
 def exchange_timestamps_thread(other_uuid: str, other_ip: str, other_tcp_port: int):
     """
     Open a connection to the other_ip, other_tcp_port
     and do the steps to exchange timestamps.
-
     Then update the neighbor_info map using other node's UUID.
     """
     print_yellow(f"ATTEMPTING TO CONNECT TO {other_uuid}")
-    pass
+    try:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.connect((other_ip,other_tcp_port))
+        print_yellow('connection stablished')
+        
+        packet = tcp.recv(4096)
+        time_current = time.time()
+        time_previous = struct.unpack('!d',packet)[0]
+        print_green(time_previous)
+        delay = time_current - time_previous
+        tcp.close()
+        
+        # put in hash table or update if it is in
+        if other_uuid in neighbor_information.keys():
+            # update
+            node_info,broadcast_count = neighbor_information[other_uuid]
+            if broadcast_count == 10:
+                # reset count and update delay
+                broadcast_count = 0
+                node_info.delay = delay
+                node_info.last_timestamp = time_current
+            else:
+                broadcast_count += 1
+        else:
+            # make new object
+            node_info = NeighborInfo(delay,time_current,other_ip,other_tcp_port)
+            broadcast_count = 0
+        neighbor_information.update({other_uuid:(node_info,broadcast_count)})
 
+    except ConnectionRefusedError:
+        print_yellow('node is down atm')
 
 def daemon_thread_builder(target, args=()) -> threading.Thread:
     """
@@ -111,12 +164,11 @@ def daemon_thread_builder(target, args=()) -> threading.Thread:
     return th
 
 
+
 def entrypoint():
-    pass
-
-############################################
-############################################
-
+    node  = daemon_thread_builder(target=send_broadcast_thread)
+    node.start()
+    receive_broadcast_thread()
 
 def main():
     """
